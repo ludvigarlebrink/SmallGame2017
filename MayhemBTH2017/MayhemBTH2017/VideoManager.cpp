e7daa7a8761ef970eb855a2c580bc9fa95d8cf31
Ittis <itamar.kup@gmail.com>
2017-03-31 14:24:02 +0200

    Hej Erik! har nu fixat lite med deferred och SDL :D och ludde bjuder på lingontårta :)

--- MayhemBTH2017/MayhemBTH2017/VideoManager.cpp
+++ MayhemBTH2017/MayhemBTH2017/VideoManager.cpp
@@ -27,8 +27,7 @@ void VideoManager::StartUp()
 
 void VideoManager::ShutDown()
 {
-	SDL_DestroyWindow(m_window);
-	SDL_Quit();
+	DisplayShutDown();
 }
 
 
@@ -43,6 +42,26 @@ glm::vec2 VideoManager::GetScreenResolution() const
 	return glm::vec2();
 }
 
+bool VideoManager::GetIsClosed()const
+{
+	return m_isClosed;
+}
+
+int VideoManager::GetScreenHeight()const
+{
+	return m_screenHeight;
+}
+
+int VideoManager::GetScreenWidth()const
+{
+	return m_screenWidth;
+}
+
+SDL_Window* VideoManager::GetWindow()const
+{
+	return m_window;
+}
+
 
 //::.. HELPER FUNCTIONS ..:://
 void VideoManager::Init()
@@ -50,15 +69,7 @@ void VideoManager::Init()
 	// Init SDL.
 	SDL_Init(SDL_INIT_VIDEO);
 
-	// Create the application window.
-	m_window = SDL_CreateWindow(
-		"Super Amazeballs Game",
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		640,
-		480,
-		SDL_WINDOW_OPENGL
-	);
+	DisplayCreate(800, 600, "test");
 
 	// Window debugging.
 
@@ -73,3 +84,85 @@ void VideoManager::Init()
 #endif
 
 }
+
+#ifdef _DEBUG
+
+static void APIENTRY openglCallbackFunction(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei lenght, const GLchar* message, const void* userParam)
+{
+	std::string output = message;
+	output += "\n";
+	OutputDebugStringA((output).c_str());
+}
+
+#endif
+
+void VideoManager::DisplayCreate(int width, int height, const std::string& title)
+{
+	m_screenWidth = width;
+	m_screenHeight = height;
+
+	SDL_GL_SetAttribute(
+		SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_DEBUG_FLAG
+	);
+
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BUFFER_SIZE, 32);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+	m_window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED
+		, width, height, SDL_WINDOW_OPENGL);
+
+	m_glContext = SDL_GL_CreateContext(m_window);
+	GLenum status = glewInit();
+
+#ifdef _DEBUG
+
+	if (status != GLEW_OK)
+	{
+		std::cout << "GLEW STATUS FAIL\n";
+	}
+
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+	glEnable(GL_DEBUG_OUTPUT);
+	glDebugMessageCallback(openglCallbackFunction, nullptr);
+	glDebugMessageControl(
+		GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, NULL, true);
+
+#endif
+
+	SDL_SetRelativeMouseMode(SDL_TRUE);
+	m_isClosed = false;
+}
+
+void VideoManager::DisplayShutDown()
+{
+	SDL_GL_DeleteContext(m_glContext);
+	SDL_DestroyWindow(m_window);
+	SDL_Quit();
+
+}
+
+void VideoManager::DisplayUpdate()
+{
+	SDL_GL_SwapWindow(m_window);
+}
+
+//::.. SET FUNCTIONS ..:://
+
+void VideoManager::SetIsClosed(bool x)
+{
+	m_isClosed = x;
+}
+
+void VideoManager::SetScreenHeight(int x)
+{
+	m_screenHeight = x;
+}
+
+void VideoManager::SetScreenWidth(int x)
+{
+	m_screenWidth = x;
+}
+
