fb0ecfdf09a8c0640747145fec81534a1ee9ebab
Ittis <itamar.kup@gmail.com>
2017-04-03 13:47:37 +0200

    asd

---
+++ MayhemBTH2017/MayhemBTH2017/ShaderDebug.h
@@ -0,0 +1,18 @@
+#ifndef __SHADERDEBUG_H__
+#define __SHADERDEBUG_H__
+
+#include "AShader.h"
+class ShaderDebug :
+	public AShader
+{
+public:
+	//::..CONSTRUCTORS..:://
+	ShaderDebug();
+	virtual ~ShaderDebug();
+
+	void Update();
+private:
+private:
+};
+
+#endif // !__SHADERDEBUG_H__
