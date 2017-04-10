41ae7298379e0758d2bdfc837c8122993a19f908
Ittis <itamar.kup@gmail.com>
2017-04-10 09:49:04 +0200

    yyy

--- MayhemBTH2017/MayhemBTH2017/System.cpp
+++ MayhemBTH2017/MayhemBTH2017/System.cpp
@@ -29,8 +29,6 @@ void System::Run()
 
 	while (isRunning)
 	{
-		//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		//glClearColor(0.3f, 0.3f, 0.7f, 1.0f);
 		msaa.Reset();
 
 		m_inputManager->Update();
