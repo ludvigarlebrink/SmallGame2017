332243d09fd714b360825bf947aba37b37c88ae0
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2017-04-10 10:46:32 +0200

    awdawd

--- MayhemBTH2017/MayhemBTH2017/LevelMarker.cpp
+++ MayhemBTH2017/MayhemBTH2017/LevelMarker.cpp
@@ -10,3 +10,16 @@ LevelMarker::LevelMarker()
 LevelMarker::~LevelMarker()
 {
 }
+
+//::.. SET FUNCTIONS ..:://
+void LevelMarker::SetMarkerMode(uint32_t markerMode)
+{
+
+
+}
+
+//::.. GET FUNCTIONS ..:://
+uint32_t LevelMarker::GetMarkerMode()
+{
+	return uint32_t();
+}
--- MayhemBTH2017/MayhemBTH2017/LevelMarker.h
+++ MayhemBTH2017/MayhemBTH2017/LevelMarker.h
@@ -30,9 +30,15 @@ public:
 	LevelMarker();
 	virtual ~LevelMarker();
 
-	void SetMarkerMode();
+	//::.. MODIFY FUNCTIONS ..:://
 	void MoveUp(float speed);
 	void MoveRight(float speed);
+
+	//::.. SET FUNCTIONS ..:://
+	void SetMarkerMode(uint32_t markerMode);
+
+	//::.. GET FUNCTIONS ..:://
+	uint32_t GetMarkerMode();
 
 	void Update();
 	void Render();
@@ -49,7 +55,6 @@ private:
 	Camera				m_camera;
 	Mesh				m_mesh;
 
-	Timer				m_timer;
 	TextureImporter		m_textureTemp;
 	Texture				m_texture;
 
