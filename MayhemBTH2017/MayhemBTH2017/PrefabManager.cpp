9f8615554f2c586c59b21d15e43f4e10c5b43c77
Ittis <itamar.kup@gmail.com>
2017-04-19 14:32:46 +0200

    Jag o lukkeboy ska festa på yom!

--- MayhemBTH2017/MayhemBTH2017/PrefabManager.cpp
+++ MayhemBTH2017/MayhemBTH2017/PrefabManager.cpp
@@ -1,6 +1,6 @@
 #include "PrefabManager.h"
 
-
+#include "AnimClip.h"
 
 uint32_t PrefabManager::numPrefabs = 0;
 Prefab * PrefabManager::prefabs = nullptr;
@@ -35,6 +35,25 @@ Prefab * PrefabManager::Instantiate(const char * name)
 //	m_handler->Import(filepath);
 	mr::MrHandler handler;
 //	handler.GetSkelHandlers()->
+
+	//handler.Import(filepath);
+	glm::vec3 test;
+
+	mr::MrMeshHandler meshHandler;
+	meshHandler.Import(filepath);
+	//test = meshHandler.GetPositions();
+
+
+
+	mr::MrAnimHandler * anim = handler.GetAnimHandlers();
+
+	//KeyFrame key;
+	//key.localTx = anim[0].GetKeyFramedJoints()->matrix;
+	//Prefab pre;
+	//Mesh mesh;
+	//pre.SetMesh(mesh);
+
+
 
 	return nullptr;
 }
