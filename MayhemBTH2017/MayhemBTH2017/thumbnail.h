e390d25626372b21c8c80effbcc68a8d307b33aa
Ittis <itamar.kup@gmail.com>
2017-05-04 08:42:35 +0200

    jhello

--- MayhemBTH2017/MayhemBTH2017/thumbnail.h
+++ MayhemBTH2017/MayhemBTH2017/thumbnail.h
@@ -2,23 +2,38 @@
 #define __THUMBNAIL_H__
 
 #include <vector>
+#include <fstream>
 
-struct
+#include <glew.h>
+
+typedef union PixelInfo
 {
-	std::uint8_t R, G, B;
-
-};
+	std::uint32_t Color;
+	struct 
+	{
+		std::uint8_t R, G, B, A;
+			 
+	};
+}*PPixelInfo;
 
 class thumbnail
 {
 public:
-	thumbnail();
+	thumbnail(const char* FilePath);
 	virtual ~thumbnail();
-	
+
+	std::vector<std::uint8_t> GetPixels()const;
+	std::uint32_t GetWidth()const;
+	std::uint32_t GetHeight()const;
+	bool HasAlphaChannel();
+
 	void Import();
 
 private:
-
+	std::vector<std::uint8_t> m_pixels;
+	std::uint32_t m_width, m_height, m_size, m_bitsPerPixel;
+	GLuint *textureData;
+	GLuint m_texture = 1;
 };
 
 
