1e7ec46365dc8f0b9f93ff0be4b4b7ec73d0d763
Ittis <itamar.kup@gmail.com>
2017-05-09 14:10:11 +0200

    PostProcessing fear luddeboii

--- MayhemBTH2017/MayhemBTH2017/PostProcessing.h
+++ MayhemBTH2017/MayhemBTH2017/PostProcessing.h
@@ -7,29 +7,36 @@
 class PostProcessing
 {
 public:
+	enum PostProcessingStates
+	{
+		NOTHING = 0,
+		SHAKE = 1,
+		CHAOS = 2,
+	};
+
 	PostProcessing();
 	virtual ~PostProcessing();
 
 	void StartUp();
 	void ShutDown();
 
-	bool* GetBools();
+	static bool IsShaking();
+	static bool IsChaos();
 
 	//::..HELPFUNCTIONS..:://
-	void StartTimer();
-	void Shake();
-	void Update(int state);
+	static void StartTimer();
+	static void Shake();
+	static void Update(int state);
 
 	static PostProcessing * Get();
 private:
 	static PostProcessing * m_instance;
-	float currentFrame;
-	float m_deltaTime = 0.0f;
-	float m_lastFrame = 0.0f;
-	GLfloat m_shakeTime = 0.0f;
-	bool m_shake = false;
-	bool m_chaos = false;
-	bool m_bools[2] = { m_shake, m_chaos };
+	static float m_currentFrame;
+	static float m_deltaTime;
+	static float m_lastFrame;
+	static float m_shakeTime;
+	static bool m_shake;
+	static bool m_chaos;
 };
 
 #endif // !__POSTPROCESSING_H__
