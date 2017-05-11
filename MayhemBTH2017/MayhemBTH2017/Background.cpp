ed0ef55b8df3e0063e6facfb087c605a86353fb2
ludvigarlebrink <ludvig.arlebrink@outlook.com>
2017-05-11 15:50:07 +0200

    baj

--- MayhemBTH2017/MayhemBTH2017/Background.cpp
+++ MayhemBTH2017/MayhemBTH2017/Background.cpp
@@ -44,13 +44,36 @@ void Background::UpdateGrandCanyon()
 	float deltaHeight = VideoManager::Get()->GetHeight() / 1080.0f;
 	float deltaWidth = VideoManager::Get()->GetWidth() / 1920.0f;
 
-	m_sprites[1].SetPosition((m_sprites[1].GetPosX() - (TimeManager::GetDeltaTime() * 10.0f)), 300 * deltaHeight);
-//	if (m_sprites[1].GetPosX() < -1300.0f * deltaWidth)
-//	{
-//		m_sprites[1].SetPosition(1300.0f * deltaWidth, 300 * deltaHeight);
-//	}
+	m_sprites[1].SetPosition(m_sprites[1].GetPosX() - (TimeManager::GetDeltaTime() * 5.0f * deltaWidth), m_sprites[1].GetPosY());
+	if (m_sprites[1].GetPosX() < -1200.0f * deltaWidth)
+	{
+		m_sprites[1].SetPosition(1200.0f * deltaWidth, m_sprites[1].GetPosY());
+	}
 
-	std::cout << m_sprites[1].GetPosX() << std::endl;
+	m_sprites[2].SetPosition(m_sprites[2].GetPosX() - (TimeManager::GetDeltaTime() * 5.0f * deltaWidth), m_sprites[2].GetPosY());
+	if (m_sprites[2].GetPosX() < -1200.0f * deltaWidth)
+	{
+		m_sprites[2].SetPosition(1200.0f * deltaWidth, m_sprites[2].GetPosY());
+	}
+
+	m_sprites[3].SetPosition(m_sprites[3].GetPosX() - (TimeManager::GetDeltaTime() * 5.0f * deltaWidth), m_sprites[3].GetPosY());
+	if (m_sprites[3].GetPosX() < -1200.0f * deltaWidth)
+	{
+		m_sprites[3].SetPosition(1200.0f * deltaWidth, m_sprites[3].GetPosY());
+	}
+
+	m_sprites[4].SetPosition(m_sprites[4].GetPosX() - (TimeManager::GetDeltaTime() * 20.0f * deltaWidth), m_sprites[4].GetPosY());
+	if (m_sprites[4].GetPosX() < -1350.0f * deltaWidth)
+	{
+		m_sprites[4].SetPosition(1350.0f * deltaWidth, m_sprites[4].GetPosY());
+	}
+
+	m_sprites[5].SetPosition(m_sprites[5].GetPosX() - (TimeManager::GetDeltaTime() * 20.0f * deltaWidth), m_sprites[5].GetPosY());
+	if (m_sprites[5].GetPosX() < -1350.0f * deltaWidth)
+	{
+		m_sprites[5].SetPosition(1350.0f * deltaWidth, m_sprites[5].GetPosY());
+	}
+
 	for (uint32_t i = 0; i < 7; i++)
 	{
 		m_sprites[i].Render();
@@ -81,25 +104,25 @@ void Background::InitBackground()
 		m_sprites[0].SetTexture(".\\Assets\\Sprites\\GCBack.png");
 		m_sprites[0].SetSize(VideoManager::Get()->GetWidth(), VideoManager::Get()->GetHeight());
 
-		m_sprites[1].SetTexture(".\\Assets\\Sprites\\GCCloud5.png");
-		m_sprites[1].SetSize(234 * deltaWidth, 96 * deltaHeight);
-		m_sprites[1].SetPosition(80 * deltaWidth, -400 * deltaHeight);
+		m_sprites[1].SetTexture(".\\Assets\\Sprites\\GCCloud3.png");
+		m_sprites[1].SetSize(175 * deltaWidth, 65 * deltaHeight);
+		m_sprites[1].SetPosition(900 * deltaWidth, 10 * deltaHeight);
 
 		m_sprites[2].SetTexture(".\\Assets\\Sprites\\GCCloud2.png");
 		m_sprites[2].SetSize(272 * deltaWidth, 116 * deltaHeight);
-		m_sprites[2].SetPosition(80 * deltaWidth, -300 * deltaHeight);
+		m_sprites[2].SetPosition(-900 * deltaWidth, 10 * deltaHeight);
 
-		m_sprites[3].SetTexture(".\\Assets\\Sprites\\GCCloud3.png");
-		m_sprites[3].SetSize(175 * deltaWidth, 65 * deltaHeight);
-		m_sprites[3].SetPosition(80 * deltaWidth, -400 * deltaHeight);
+		m_sprites[3].SetTexture(".\\Assets\\Sprites\\GCCloud5.png");
+		m_sprites[3].SetSize(234 * deltaWidth, 96 * deltaHeight);
+		m_sprites[3].SetPosition(0 * deltaWidth, 100 * deltaHeight);
 
 		m_sprites[4].SetTexture(".\\Assets\\Sprites\\GCCloud1.png");
 		m_sprites[4].SetSize(500 * deltaWidth, 242 * deltaHeight);
-		m_sprites[4].SetPosition(80 * deltaWidth, 300 * deltaHeight);
+		m_sprites[4].SetPosition(-700 * deltaWidth, 200 * deltaHeight);
 
 		m_sprites[5].SetTexture(".\\Assets\\Sprites\\GCCloud4.png");
 		m_sprites[5].SetSize(762 * deltaWidth, 360 * deltaHeight);
-		m_sprites[5].SetPosition(80 * deltaWidth, 300 * deltaHeight);
+		m_sprites[5].SetPosition(700 * deltaWidth, 300 * deltaHeight);
 
 		m_sprites[6].SetTexture(".\\Assets\\Sprites\\GCFront.png");
 		m_sprites[6].SetSize(VideoManager::Get()->GetWidth(), VideoManager::Get()->GetHeight());
